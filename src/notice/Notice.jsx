f0af2b5670bd07f8081d9dae69a0bab14977b585
G-Hae <sifialee@gmail.com>
2022-09-12 11:15:09 +0900

    feat: notice 태그문제 해결

--- src/notice/Notice.jsx
+++ src/notice/Notice.jsx
@@ -18,7 +18,6 @@ function Notice() {
 
   return (
     <>
-
     {!cookies? (<WorkSpaceErrorModal/>
     ):(
       <StWrapper>
@@ -32,12 +31,14 @@ function Notice() {
               <>
 
             {notice?.map((data, i) => {
+
               return(
                 <StNoticeContainer>
                 <StTitle fc="#00a99d">공지사항</StTitle>
                   <StNoticeBox key={notice.id}>
                       <StTitle fc="#333333">{data.title}</StTitle>
-                      <StContent> {data.content}</StContent>
+                      <StContent>
+                        <div dangerouslySetInnerHTML={{ __html: data.content} }/></StContent>   
                       <StInfoDiv>
                         <p>{data.nickname} ｜</p>
                         <p>{data.createdAt.slice(0, -13)} ｜</p>
@@ -59,61 +60,55 @@ export default Notice;
 
 const StWrapper = styled.div`
   width: 96%;
-  min-height: 30vh;
-  margin-left: 2%;
-  margin-top: 3%;
+  margin-left: 18px;
+  margin-top: 20px;
   display: flex;
   flex-direction: column;
   align-items: left;
   color: #333333;
-  font-size: 16px;
   letter-spacing: -0.8px;
 `;
 
 const StTitle = styled.p`
  color: ${(props) => props.fc};
  text-align: left;
- font-size: 1.3rem;
+ font-size: 20px;
  font-weight: bold;
  letter-spacing: -1.2px;
- margin-bottom: 15px;
 `;
 
 const StNoticeContainer = styled.div`
-  padding: 3%;
-  width: 90%;
-  min-height: 15vh;
+  padding: 25px;
   display: flex;
   flex-direction: column;
   align-items: left;
   background-color: #eef8f8;
   margin: 5px;
+  margin-bottom: 15px;
 `;
 
 const StNoticeBox = styled.div`
-  margin-top: 15px;
+  margin-top: 18px;
   margin-bottom: 15px;
   display: flex;
   flex-direction: column;
   align-items: left;
 `;
 
-
 const StContent = styled.div`
+  margin-top: 10px;
   width: 100%;
-  min-height: 5vh;
-  max-height: 20vh;
   display: flex;
   flex-direction: column;
   align-items: left;
-  font-size: 1.1rem;
-  line-height: 1.5rem;
+  font-size: 20px;
+  line-height: 25px;
   font-weight: 500;
   white-space:pre-wrap;
 `;
 
 const StInfoDiv = styled.div`
-  margin-top: 2.5%;
+  margin-top: 18px;
   display: flex;
   flex-direction: row;
   align-items: left;
@@ -121,5 +116,5 @@ const StInfoDiv = styled.div`
   font-size: 16px;
   letter-spacing: -0.8px;
   font-weight: 500;
-  font-size: 0.9rem;
+  font-size: 16px;
 `;
