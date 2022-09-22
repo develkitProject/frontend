2878843fc38ceef2d64cf362ebb26ac829dcb2a8
LuisKlopp <fbgus333@gmail.com>
2022-09-22 21:13:02 +0900

    dev_1

--- src/detail/index.jsx
+++ src/detail/index.jsx
@@ -66,7 +66,7 @@ export default function WorkspaceDetailPage() {
   // };
 
   useEffect(() => {
-    checkMember();
+    // checkMember();
   }, []);
 
   //---------------------------------------------------------------
