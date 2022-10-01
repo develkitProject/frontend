daf932886d335a9b21c0263384a7c35c4ce629b9
LuisKlopp <fbgus333@gmail.com>
2022-10-01 19:07:49 +0900

    reafactor:폴더구조정리

--- src/oauth/login/hooks/useInputLogin.js
+++ src/oauth/login/hooks/useInputLogin.js
@@ -1,7 +1,7 @@
 import { useCallback, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
-import { loginApi } from '../../../data/login';
+import { loginApi } from '../../data/login';
 import { setAccessToken } from '../../../Cookie';
 
 export default function useInputLogin() {
