74c8b5e8c5fa6dc2652608927fe043b2a5f2bbf2
LuisKlopp <fbgus333@gmail.com>
2023-05-10 14:41:54 +0900

    refactor: useChangeMenu.jsx => .tsx

--- src/detail/hooks/useChangeMenu.ts
+++ src/detail/hooks/useChangeMenu.ts
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function useChangeMenu() {
   const [menu, setMenu] = useState('home');
   const [tab, setTab] = useState('list');
   const [stateId, setStateId] = useState(0);
 
-  const onDocumentHandle = (tabpoint, docsId) => {
+  const onDocumentHandle = (tabpoint: string, docsId: number) => {
     setTab(tabpoint);
     setStateId(docsId);
   };
 
   const onClickMenu =
-    ({ key, tab, docsId }) =>
+    ({ key, tab, docsId }: {key: string, tab: string, docsId: number}) =>
     () => {
       setMenu(key);
       if (tab) {
