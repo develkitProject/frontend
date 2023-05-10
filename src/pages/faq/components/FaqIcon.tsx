d85aaf891f7751208ba92cd5c7867b37734ecbf3
LuisKlopp <fbgus333@gmail.com>
2023-05-10 15:24:28 +0900

    refactor: FAQ.jsx => FAQ.tsx

--- src/pages/faq/components/FaqIcon.tsx
+++ src/pages/faq/components/FaqIcon.tsx
@@ -13,7 +13,7 @@ const FaqIcon = {
       />
     </svg>
   ),
-  ArrowDown: ({ onClick }) => (
+  ArrowDown: ({ onClick }: {onClick: () => void}) => (
     <svg
       onClick={onClick}
       width="30"
@@ -36,7 +36,7 @@ const FaqIcon = {
       </defs>
     </svg>
   ),
-  ArrowUp: ({ onClick }) => (
+  ArrowUp: ({ onClick }: {onClick: () => void}) => (
     <svg
       onClick={onClick}
       width="30"
